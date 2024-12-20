56c9a7c9ac91004b38c333e551878850bec9d08e
neon-uriel <48177204+neon-uriel@users.noreply.github.com>
2024-12-21 00:41:09 +0900

    first commit

---
+++ src/client/java/com/neonuriel/thankyouboxmod/ThankyouBoxMODClient.java
@@ -0,0 +1,10 @@
+package com.neonuriel.thankyouboxmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThankyouBoxMODClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
