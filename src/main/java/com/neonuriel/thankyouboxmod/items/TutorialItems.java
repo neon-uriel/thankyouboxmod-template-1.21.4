9e305b4f06d0b14aa599f02a94fd75302b488f72
usiusa7991 <angel.impish.m@gmail.com>
2024-12-21 06:01:52 +0900

    feature: アイテムを食べ物化

--- src/main/java/com/neonuriel/thankyouboxmod/items/TutorialItems.java
+++ src/main/java/com/neonuriel/thankyouboxmod/items/TutorialItems.java
@@ -1,11 +1,11 @@
 package com.neonuriel.thankyouboxmod.items;
 
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
-
 import java.util.function.Function;
 
 
@@ -13,7 +13,7 @@ public class TutorialItems {
   private TutorialItems() {
   }
  
-  public static final Item CUSTOM_ITEM = register("custom_item", Item::new, new Item.Settings());
+  public static final Item CUSTOM_ITEM = register("custom_item", Item::new, new Item.Settings().food(new FoodComponent.Builder().build()));
  
   public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
     final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("tutorial", path));
