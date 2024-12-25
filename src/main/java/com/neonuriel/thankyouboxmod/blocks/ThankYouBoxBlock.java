497b7efa75bf56e5b8a6767017609345ae38fa28
usiusa7991 <angel.impish.m@gmail.com>
2024-12-25 18:01:26 +0900

    fix: モデル出し完了

--- src/main/java/com/neonuriel/thankyouboxmod/blocks/ThankYouBoxBlock.java
+++ src/main/java/com/neonuriel/thankyouboxmod/blocks/ThankYouBoxBlock.java
@@ -1,12 +1,25 @@
 package com.neonuriel.thankyouboxmod.blocks;
 
+import org.jetbrains.annotations.Nullable;
+
+import com.neonuriel.thankyouboxmod.entities.Entities;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
 
-public class ThankYouBoxBlock extends Block {
+public class ThankYouBoxBlock extends Block implements BlockEntityProvider  {
     public ThankYouBoxBlock(Settings settings) {
         super(settings);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return Entities.THANK_YOU_BOX_ENTITY.instantiate(pos, state);
     }
 
     @Override
